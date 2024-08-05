907a57d326d7a0a64f1562d8b32b6ce8557899c3
developer-eww <114571805+developer-eww@users.noreply.github.com>
2024-08-05 22:25:10 +0530

    hello

--- CMExpertise Precticle/Webservice/WebService.swift
+++ CMExpertise Precticle/Webservice/WebService.swift
@@ -17,6 +17,7 @@ class WebService {
                 let empData = try! jsonDecoder.decode(T.self, from: data)
                 completion(.success(empData))
             } else {
+                print("hello")
                 completion(.failure(ApiError.fail))
             }
         }.resume()
